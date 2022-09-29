69b11ee930683577ff9d5972af4d5d4897b98be4
Ricko48 <ondrejka.richard@gmail.com>
2022-09-29 15:57:48 +0200

    Implemented User

--- DAL/Entities/User.cs
+++ DAL/Entities/User.cs
@@ -8,6 +8,12 @@ namespace DAL.Entities
 {
     public class User
     {
-        
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
     }
 }

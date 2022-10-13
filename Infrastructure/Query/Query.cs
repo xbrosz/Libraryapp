d953553a48e8024f507b9ae466c152bf0b173944
xbrosz <485694@muni.cz>
2022-10-13 22:06:25 +0200

    Creating new Infrastructure with Interfaces (Repository, UnitOfWork and Query)

---
+++ Infrastructure/Query/Query.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Query
+{
+    public class Query
+    {
+    }
+}

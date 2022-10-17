e7f3424a4a06acfc7bdc012c013f220a45d235cd
Patrik Brosz <patri@LAPTOP-REHQ2810>
2022-10-17 23:38:01 +0200

    Implementing generic query for EF (where query)

---
+++ Infrastructure.EFCore/Query/EFGenericQuery.cs
@@ -0,0 +1,70 @@
+﻿using DAL.Data;
+using Infrastructure.EFCore.ExpressionHelpers;
+using Infrastructure.Query;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.EFCore.Query
+{
+    public class EFGenericQuery<TEntity> : GenericQuery<TEntity> where TEntity : class, new()
+    {
+        protected LibraryappDbContext DbContext { get; set; }
+
+        public EFGenericQuery(LibraryappDbContext dbContext) 
+        { 
+            DbContext = dbContext;
+        }
+        public override IEnumerable<TEntity> Execute()
+        {
+            IQueryable<TEntity> query = DbContext.Set<TEntity>();
+
+            if (WherePredicate.Capacity != 0)
+            {
+                query = ApplyWhere(query);
+            }
+
+            return query.ToList();
+        }
+
+        private IQueryable<TEntity> ApplyWhere(IQueryable<TEntity> query)
+        {
+            foreach (var expr in WherePredicate)
+            {
+                var p = Expression.Parameter(typeof(TEntity), "p");
+
+                var columnNameFromObject = typeof(TEntity)
+                    .GetProperty(expr.columnName)
+                    ?.Name;
+
+                var exprProp = Expression.Property(p, columnNameFromObject);
+
+                var expression = expr.expression;
+
+                var parameters = (IReadOnlyCollection<ParameterExpression>)expression
+                    .GetType()
+                    .GetProperty("Parameters")
+                    ?.GetValue(expression);
+
+                var body = (Expression)expression
+                    .GetType()
+                    .GetProperty("Body")
+                    ?.GetValue(expression);
+
+                var visitor = new ReplaceParamVisitor(parameters.First(), exprProp);
+
+                var exprNewBody = visitor.Visit(body);
+
+                var lambda = Expression.Lambda<Func<TEntity, bool>>(exprNewBody, p);
+
+                query = query.Where(lambda);
+            }
+
+            return query;
+        }
+    }
+}

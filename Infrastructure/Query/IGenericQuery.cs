f9cf42dffb87b5ad5e509673a0730d21b09e0eef
Patrik Brosz <patri@LAPTOP-REHQ2810>
2022-10-17 23:36:26 +0200

    Introducing IGenericQuery with Where query

---
+++ Infrastructure/Query/IGenericQuery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Query
+{
+    public interface IGenericQuery<TEntity> where TEntity : class, new()
+    {
+        IGenericQuery<TEntity> Where<T>(Expression<Func<T, bool>> rootPredicate, string columnName) where T : IComparable<T>;
+
+        IEnumerable<TEntity> Execute();
+    }
+}

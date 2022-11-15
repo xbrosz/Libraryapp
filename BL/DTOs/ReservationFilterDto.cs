b7a025cce4e932ad6b9f6deb00657e6388e1d923
Patrik Brosz <patri@LAPTOP-REHQ2810>
2022-11-15 23:22:14 +0100

    Introducing QueryObject and FilterDto for Reservation entity

---
+++ BL/DTOs/ReservationFilterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BL.DTOs
+{
+    public class ReservationFilterDto
+    {
+        public int UserId { get; set; }
+        public int? RequestedPageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
---
+++ BL/QueryObjects/ReservationQueryObject.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using BL.DTOs;
+using DAL.Data;
+using DAL.Entities;
+using Infrastructure.EFCore.Query;
+using Infrastructure.Query;
+
+namespace BL.QueryObjects
+{
+    public class ReservationQueryObject
+    {
+        private IMapper mapper;
+
+        private IGenericQuery<Reservation> myQuery;
+
+        public ReservationQueryObject(IMapper mapper, LibraryappDbContext dbx)
+        {
+            this.mapper = mapper;
+            myQuery = new EFGenericQuery<Reservation>(dbx);
+        }
+
+        public QueryResultDto<ReservationsDto> ExecuteQuery(ReservationFilterDto filter)
+        {
+            var query = myQuery.Where<int>(a => a == filter.UserId, "UserId");
+
+            if (filter.RequestedPageNumber.HasValue)
+            {
+                query = query.Page(filter.RequestedPageNumber.Value, filter.PageSize);
+            }
+
+            return mapper.Map<QueryResultDto<ReservationsDto>>(query.Execute());
+        }
+    }
+}

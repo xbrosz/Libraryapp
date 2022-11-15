1ce5183c6272e135cc0172957d06a8fcd9af7ee7
Patrik Brosz <patri@LAPTOP-REHQ2810>
2022-11-15 23:23:36 +0100

    Implementing getReservationByUserId in ReservationService

--- BL/Services/ReservationService.cs
+++ BL/Services/ReservationService.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using BL.DTOs;
+using BL.QueryObjects;
+using BL.Services;
+using DAL.Data;
+using DAL.Entities;
+using Infrastructure.Repository;
 
 namespace BL.Service
 {
-    internal class ReservationService
+    public class ReservationService : GenericService<Reservation, ReservationsDto, ReservationsDto>, IReservationService
     {
+        private IMapper mapper = new Mapper(new MapperConfiguration(MappingConfig.ConfigureMapping));
+        private IRepository<Reservation> repository;
+        private LibraryappDbContext dbContext;
+        private ReservationQueryObject queryObject;
+
+        public ReservationService(IRepository<Reservation> repository, LibraryappDbContext ctx) : base(repository)
+        {
+            this.repository = repository;
+            this.dbContext = ctx;
+        }
+
+        public IEnumerable<ReservationsDto> getReservationsByUserId(int userId)
+        {
+            queryObject = new ReservationQueryObject(mapper, dbContext);
+
+            return queryObject.ExecuteQuery(new ReservationFilterDto() { UserId = userId }).Items;
+        }
+
     }
 }

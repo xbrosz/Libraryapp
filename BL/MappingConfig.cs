3cafa6695b2bdd20501e01c0d3a19e04d436131a
Patrik Brosz <patri@LAPTOP-REHQ2810>
2022-11-16 15:43:42 +0100

    Adding mappring for User->UserDetailDto

--- BL/MappingConfig.cs
+++ BL/MappingConfig.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
-using BL.DTOs;
+using BL.DTOs.Reservation;
+using BL.DTOs.User;
 using DAL.Entities;
 
 namespace BL
@@ -9,7 +10,8 @@ namespace BL
         public static void ConfigureMapping(IMapperConfigurationExpression config)
         {
             config.CreateMap<Reservation, ReservationsDto>()
-                .ForMember(dest => dest.BookTitle, act => act.MapFrom(src => src.BookPrint.Book.Title));
+                .ForMember(dest => dest.BookTitle, act => act.MapFrom(src => src.BookPrint.Book.Title)).ReverseMap();
+            config.CreateMap<User, UserDetailDto>().ReverseMap();
         }
     }
 }

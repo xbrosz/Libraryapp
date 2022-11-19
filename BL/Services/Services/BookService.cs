de249f2377f99ea00409558e4c75dbf01164cb4d
Lukas Hasik <xhasik@fi.muni.cz>
2022-11-19 23:02:02 +0100

    fixed local conflicts

--- BL/Services/Services/BookService.cs
+++ BL/Services/Services/BookService.cs
@@ -1,28 +1,30 @@
 ﻿using AutoMapper;
 using BL.DTOs;
 using BL.QueryObjects;
+using BL.QueryObjects.IQueryObject;
+using BL.QueryObjects.QueryObjects;
 using BL.Services.GenericService;
 using BL.Services.IServices;
 using DAL.Data;
 using DAL.Entities;
 using Infrastructure.Repository;
+using Infrastructure.UnitOfWork;
 
 namespace BL.Services.Services
 {
     public class BookService : GenericService<Book, BookDetailDto, BookPrintDto, BookDetailDto>, IBookService
     {
-        private LibraryappDbContext _context;
-        
+        IQueryObject<BookFilterDto, BookDetailDto> _bookQueryObject;
 
-        public BookService(LibraryappDbContext context, IRepository<Book> bookRepository) : base(bookRepository)
+        public BookService(IUnitOfWork unitOfWork, IMapper mapper, IQueryObject<BookFilterDto,BookDetailDto> bookQueryObject) : base(unitOfWork, mapper, unitOfWork.BookRepository)
         {
-            _context = context;
 
+            _bookQueryObject = bookQueryObject;
         }
 
         public IEnumerable<BookGridDto> GetBookbyAuthorID(int authorID)
         {
-            var bookQueryObject = new BookQueryObject(mapper, _context);
+            var bookQueryObject = new BookQueryObject(_mapper);
 
             return bookQueryObject.ExecuteQuery(new BookFilterDto
             {
@@ -33,7 +35,7 @@ namespace BL.Services.Services
 
         public BookDetailDto GetBookDetailByID(int bookID)
         {
-            return mapper.Map<BookDetailDto>(find(bookID));
+            return this._mapper.Map<BookDetailDto>(Find(bookID));
         }
 
     }

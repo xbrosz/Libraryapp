7a055388cf52ac79f7c95993fe30cfd998778ad3
xbrosz <patri@LAPTOP-REHQ2810>
2022-11-20 00:53:30 +0100

    Adding inheritance from IGenericService

--- BL/Services/IServices/IBookPrintService.cs
+++ BL/Services/IServices/IBookPrintService.cs
@@ -1,10 +1,11 @@
 ﻿using BL.DTOs;
 using BL.DTOs.Reservation;
-
+using BL.Services.GenericService;
+using DAL.Entities;
 
 namespace BL.Services.IServices
 {
-    public interface IBookPrintService
+    public interface IBookPrintService : IGenericService<BookPrint, BookPrintDto, BookPrintDto, BookPrintDto>
     {
         public IEnumerable<BookPrintDto> GetBookbyBranchIDAndBookID(int branchId, int bookId);
         public IEnumerable<BookPrintDto> GetBookbyBranchID(int branchId);

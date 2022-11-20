da4e9ef19c6af49647abb01494f3260b1a4a7ba1
xbrosz <patri@LAPTOP-REHQ2810>
2022-11-20 22:44:27 +0100

    Adding reservations filtering tests

--- BL.Tests/ReservationServiceTests.cs
+++ BL.Tests/ReservationServiceTests.cs
@@ -64,5 +64,79 @@ namespace BL.Tests
             _queryObjectMock.Verify(x => x.ExecuteQuery(It.IsAny<ReservationFilterDto>()), Times.Once);
             _uowMock.Verify(x => x.ReservationRepository, Times.Once);
         }
+
+        [Fact]
+        public void FilterReservationsByUserId()
+        {
+            var mapper = new Mapper(new MapperConfiguration(MappingConfig.ConfigureMapping));
+
+            var resultDto = new ReservationsDto()
+            {
+                Id = 1,
+                BookPrintId = 1,
+                BookTitle = "title"
+            };
+
+            var queryResult = new QueryResultDto<ReservationsDto>()
+            {
+                Items = new List<ReservationsDto>() { resultDto },
+                TotalItemsCount = 1
+            };
+
+            _queryObjectMock
+                .Setup(x => x.ExecuteQuery(It.IsAny<ReservationFilterDto>()))
+                .Returns(queryResult);
+
+            _uowMock
+                .Setup(x => x.ReservationRepository)
+                .Returns(_repoMock.Object);
+
+            var service = new ReservationService(_uowMock.Object, mapper, _queryObjectMock.Object);
+
+            var res = service.GetReservationsByUserId(1);
+
+            Assert.Single(res);
+            Assert.Equal(res.First(), resultDto);
+
+            _queryObjectMock.Verify(x => x.ExecuteQuery(It.IsAny<ReservationFilterDto>()), Times.Once);
+            _uowMock.Verify(x => x.ReservationRepository, Times.Once);
+        }
+
+        [Fact]
+        public void FilterReservationsByDateRange()
+        {
+            var mapper = new Mapper(new MapperConfiguration(MappingConfig.ConfigureMapping));
+
+            var resultDto = new ReservationsDto()
+            {
+                Id = 1,
+                BookPrintId = 1,
+                BookTitle = "title"
+            };
+
+            var queryResult = new QueryResultDto<ReservationsDto>()
+            {
+                Items = new List<ReservationsDto>() { resultDto },
+                TotalItemsCount = 1
+            };
+
+            _queryObjectMock
+                .Setup(x => x.ExecuteQuery(It.IsAny<ReservationFilterDto>()))
+                .Returns(queryResult);
+
+            _uowMock
+                .Setup(x => x.ReservationRepository)
+                .Returns(_repoMock.Object);
+
+            var service = new ReservationService(_uowMock.Object, mapper, _queryObjectMock.Object);
+
+            var res = service.GetReservationsInDateRangeByBookAndBranch(1, 1, DateTime.Now, DateTime.Now);
+
+            Assert.Single(res);
+            Assert.Equal(res.First(), resultDto);
+
+            _queryObjectMock.Verify(x => x.ExecuteQuery(It.IsAny<ReservationFilterDto>()), Times.Once);
+            _uowMock.Verify(x => x.ReservationRepository, Times.Once);
+        }
     }
 }

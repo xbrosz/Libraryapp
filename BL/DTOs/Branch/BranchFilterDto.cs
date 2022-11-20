db7c39cab02584f6904664848ff94efe19f8752e
Lukáš Hasík <lukashasik@email.cz>
2022-11-20 22:35:10 +0100

    Added test calsses for book and bookprint services

--- BL/DTOs/Branch/BranchFilterDto.cs
+++ BL/DTOs/Branch/BranchFilterDto.cs
@@ -8,12 +8,10 @@ namespace BL.DTOs.Branch
 {
     public class BranchFilterDto
     {
-        public string Name { get; set; }
-        public string Address { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
         public int? RequestedPageNumber { get; set; }
         public int PageSize { get; set; }
-        public string SortCriteria { get; set; }
-        public bool SortAscending { get; set; }
     }
 }
 

2cbc12ee37c68ecde9dfbf63ef67fac540fd68c4
Lukáš Hasík <lukashasik@email.cz>
2022-11-20 23:36:51 +0100

    adjusted mapping config for bookdetaildto

--- BL/MappingConfig.cs
+++ BL/MappingConfig.cs
@@ -19,7 +19,9 @@ namespace BL
             config.CreateMap<User, CreateUserDto>().ReverseMap();
             config.CreateMap<Rating, RatingDto>().ReverseMap();
             config.CreateMap<Author, AuthorDto>().ReverseMap();
-            config.CreateMap<Book, BookDetailDto>().ReverseMap();
+            config.CreateMap<Book, BookDetailDto>().ForMember(dest => dest.AuthorName, act => act.MapFrom(src => src.Author.FirstName
+                                                                                                                + src.Author.MiddleName
+                                                                                                                + src.Author.LastName)).ReverseMap();
             config.CreateMap<Book, BookGridDto>().ReverseMap();
             config.CreateMap<BookPrint, BookPrintDto>().ReverseMap();
             config.CreateMap<Branch, BranchDto>().ReverseMap();

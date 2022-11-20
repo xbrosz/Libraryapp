ef7d3d06f63e5ec216b8bb16e0094868e3e17154
xbrosz <patri@LAPTOP-REHQ2810>
2022-11-20 21:08:03 +0100

    Introducing UserServiceTests

---
+++ BL.Tests/UserServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using BL.DTOs;
+using BL.DTOs.User;
+using BL.Hasher;
+using BL.QueryObjects.IQueryObject;
+using BL.Services.Services;
+using DAL.Entities;
+using Infrastructure.Repository;
+using Infrastructure.UnitOfWork;
+
+namespace BL.Tests
+{
+    public class UserServiceTests
+    {
+        Mock<IUnitOfWork> _uowMock;
+        Mock<IQueryObject<UserFilterDto, UserDetailDto>> _queryObjectMock;
+        Mock<IRepository<User>> _repoMock;
+
+        public UserServiceTests()
+        {
+            _uowMock = new Mock<IUnitOfWork>();
+            _queryObjectMock = new Mock<IQueryObject<UserFilterDto, UserDetailDto>>();
+            _repoMock = new Mock<IRepository<User>>();
+        }
+    }
+}

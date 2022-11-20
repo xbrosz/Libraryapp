02144ff24727b04e84a855c510afded504393a2f
Lukáš Hasík <lukashasik@email.cz>
2022-11-21 00:13:42 +0100

    fixed ratingservice

---
+++ BL/Services/IServices/IRatingService.cs
@@ -0,0 +1,19 @@
+﻿using BL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Services.IServices
+{
+    public interface IRatingService
+    {
+        public double GetBookAverageRating(int bookId);
+
+        public IEnumerable<RatingDto> GetRatingsByBook(int bookId);
+
+
+
+    }
+}

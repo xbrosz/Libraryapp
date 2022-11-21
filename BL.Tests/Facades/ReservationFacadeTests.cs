e0fed6270da3bbb6ce8fd728bf893329fb108246
xbrosz <patri@LAPTOP-REHQ2810>
2022-11-21 02:12:33 +0100

    Introducing ReservationFacadeTests class

---
+++ BL.Tests/Facades/ReservationFacadeTests.cs
@@ -0,0 +1,26 @@
+﻿using Autofac.Core;
+using BL.DTOs;
+using BL.DTOs.Reservation;
+using BL.Facades;
+using BL.Services.IServices;
+using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Tests.Facades
+{
+    public class ReservationFacadeTests
+    {
+        Mock<IReservationService> _reservationServiceMock;
+        Mock<IBookPrintService> _bpServiceMock;
+        public ReservationFacadeTests()
+        {
+            _reservationServiceMock = new Mock<IReservationService>();
+            _bpServiceMock = new Mock<IBookPrintService>();
+        }
+
+    }
+}

f30ce8777b9e0ab3958fdd6dc2a24d9d260f460a
xbrosz <patri@LAPTOP-REHQ2810>
2023-01-15 23:32:58 +0100

    Fixing date formatting

--- FE/Models/NewReservationModel.cs
+++ FE/Models/NewReservationModel.cs
@@ -1,5 +1,6 @@
 ﻿using BL.DTOs.Reservation;
 using BL.Facades.Facades;
+using System.ComponentModel.DataAnnotations;
 
 namespace FE.Models
 {
@@ -10,7 +11,11 @@ namespace FE.Models
         public IEnumerable<string> Branches { get; set; }
         public string BookTitle { get; set; }
         public string SelectedBranch { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}", ApplyFormatInEditMode = true)]
         public string FromDate { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}", ApplyFormatInEditMode = true)]
         public string ToDate { get; set; }
 
     }
